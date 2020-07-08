3acc6f1c2e392476ac8c7fc7e582f9e329bc13a9
hopdt <hopdt@ghtk.vn>
2020-07-08 16:21:04 +0700

    Done Beginner Tasks List

--- quickstart/resources/views/common/errors.blade.php
+++ quickstart/resources/views/common/errors.blade.php
@@ -1,2 +1,14 @@
-Link:  	https://laravel.com/docs/5.1/quickstart
-Position: The @include('common.errors') directive will load the template located
+@if (count($errors) > 0)
+    <!-- Form Error List -->
+    <div class="alert alert-danger">
+        <strong>Whoops! Something went wrong!</strong>
+
+        <br><br>
+
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
--- quickstart/routes/web.php
+++ quickstart/routes/web.php
@@ -1,7 +1,7 @@
 <?php
-
+use App\Task;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/**
+ * Display All Tasks
+ */
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
 });
+
+/**
+ * Add A New Task
+ */
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+
+/**
+ * Delete An Existing Task
+ */
+Route::delete('/task/{id}', function ($id) {
+    $task = Task::findOrFail($id)->delete();
+
+    return redirect('/');
+});
+
